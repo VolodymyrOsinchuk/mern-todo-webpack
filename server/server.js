0ac75936e97b3c282957e3e1f4e0b134802ad712
VolodymyrOsinchuk <osinchuk28@live.com>
2025-08-08 12:24:51 +0200

    reaparation

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const path = require('path') // Assurez-vous que path est importé
+const path = require('path')
 
 const todoRoutes = require('./routes/todo')
 const connectDB = require('./config/db')
@@ -20,18 +20,17 @@ app.use('/api/todos', todoRoutes)
 
 // Ce bloc ne s'exécute que pour le développement local
 if (!process.env.VERCEL) {
-  // Sert les fichiers statiques du client React
   app.use(express.static(path.join(__dirname, '../client/dist')))
 
-  // La route catch-all : pour toute requête qui ne correspond pas à l'API,
-  // renvoyer l'index.html de React.
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'))
   })
+  console.log(path.join(__dirname, '../client/dist', 'index.html'))
 
   const PORT = process.env.PORT || 5000
+
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running for local development on port ${PORT}`)
   })
 }
 
